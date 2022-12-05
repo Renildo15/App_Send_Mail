18d7dbccef66e343ed994cf8c6dd5db170127311
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-12-05 09:25:31 -0300

    envio com base nos parametros

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -48,16 +48,16 @@
 			//Server settings
 			$mail->SMTPDebug = 2;                      //Enable verbose debug output
 			$mail->isSMTP();                                            //Send using SMTP
-			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+			$mail->Host       = 'smtp.cconsistwe.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'teste@gmail.com';                     //SMTP username
-			$mail->Password   = 'senha';                               //SMTP password
+			$mail->Username   = 'deedee@cconsistwe.com';                     //SMTP username
+			$mail->Password   = '878d15333b2deeed39f960adf8078960';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 			//Recipients
-			$mail->setFrom('teste@gmail.com', 'Teste');
-			$mail->addAddress('teste@gmail.com', 'Teste');     //Add a recipient
+			$mail->setFrom('deedee@cconsistwe.com', 'Teste');
+			$mail->addAddress($mensagem->__get('para'));     //Add a recipient
 			//$mail->addAddress('ellen@example.com');               //Name is optional
 			//$mail->addReplyTo('info@example.com', 'Information');
 			//$mail->addCC('cc@example.com');
@@ -69,12 +69,12 @@
 
 			//Content
 			$mail->isHTML(true);                                  //Set email format to HTML
-			$mail->Subject = 'Here is the subject';
-			$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-			$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+			$mail->Subject = $mensagem->__get('assunto');
+			$mail->Body    = $mensagem->__get('mensagem');
+			//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 			$mail->send();
-			echo 'Message has been sent';
+			echo 'Email enviado com sucesso!';
 	} catch (Exception $e) {
 			echo "Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde.";
 			echo 'Detalhes do erro: ' . $mail->ErrorInfo;
